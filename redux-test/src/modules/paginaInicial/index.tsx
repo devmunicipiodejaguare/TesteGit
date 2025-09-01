78dfec3b9ae96be07ecbbd54b6aabf9d8a96c261
devmunicipiodejaguare <124713388+devmunicipiodejaguare@users.noreply.github.com>
2025-09-01 09:59:33 -0300

    novo

--- redux-test/src/modules/paginaInicial/index.tsx
+++ redux-test/src/modules/paginaInicial/index.tsx
@@ -1,28 +1,44 @@
-import { useState } from "react";
-import { Styled } from "./styled";
-import { useDispatch } from "react-redux";
-import { armazenarNumero } from "../../store/slices/counterSlice";
-import { useSelector } from "react-redux";
-import type { RootState } from "../../store";
+import {
+  Styled,
+  StyledTable,
+  StyledThead,
+  StyledTbody,
+  StyledTd,
+  StyledTh,
+  StyledTr,
+  StyledThGroup,
+  StyledTrContaier,
+  StyledThContainer,
+} from "./styled";
 
 export default function TesteStorege() {
-  const [numero, setNumero] = useState(0);
-  const dispatch = useDispatch();
-  const valor = useSelector((state: RootState) => state.counter.value);
-
   return (
     <Styled>
-      <div>Teste para armazenar numeros</div>
-      <input
-        type="number"
-        value={numero}
-        onChange={(e) => setNumero(Number(e.target.value))}
-      />
-      <button onClick={() => dispatch(armazenarNumero(numero))}>
-        Armazenar
-      </button>
-      <div>Exibir valores:</div>
-      <div>{valor}</div>
+      <StyledTable>
+        <StyledThead>
+          <StyledTr>
+            <StyledTh rowSpan={2}>Nº</StyledTh>
+            <StyledTh rowSpan={2}>Nome</StyledTh>
+            <StyledTh rowSpan={2}>Nascimento</StyledTh>
+            <StyledThGroup largura={20}>Teste</StyledThGroup>
+          </StyledTr>
+          <StyledTrContaier>
+            <StyledThContainer>
+              <div>
+                <h3>Teste</h3>
+              </div>
+            </StyledThContainer>
+          </StyledTrContaier>
+        </StyledThead>
+
+        <StyledTbody>
+          <StyledTr>
+            <StyledTd>1</StyledTd>
+            <StyledTd>Nome</StyledTd>
+            <StyledTd>01/01/2000</StyledTd>
+          </StyledTr>
+        </StyledTbody>
+      </StyledTable>
     </Styled>
   );
 }
--- redux-test/src/modules/paginaInicial/styled.ts
+++ redux-test/src/modules/paginaInicial/styled.ts
@@ -1,8 +1,45 @@
 import styled from "styled-components";
 
 export const Styled = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
+  width: 50%;
+  height: 50%;
+  display: flex;
+  align-items: start;
+  justify-content: start;
+`;
+
+export const StyledTable = styled.table`
+  width: 100%;
+  height: 20%;
+  border: 1px solid #fff;
+  padding: 0;
+  margin: 0;
+  border-collapse: collapse;
+  /* table-layout: fixed; */
+`;
+export const StyledThead = styled.thead``;
+
+export const StyledTh = styled.th`
+  border: 1px solid #fff;
+`;
+
+export const StyledTbody = styled.tbody``;
+
+export const StyledTr = styled.tr``;
+
+export const StyledTd = styled.td`
+  border: 1px solid #fff;
+`;
+
+export const StyledTrContaier = styled.tr`
+  width: 100%;
+`;
+export const StyledThContainer = styled.th`
+  background-color: red;
+  border: 1px solid #fff;
+
+`;
+export const StyledThGroup = styled.th<{ largura: number }>`
+  width: ${({ largura }) =>
+    largura ? `${largura}vw` : "auto"}; // Usa a largura definida no style
 `;
